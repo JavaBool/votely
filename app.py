a166f766f5d85e06d1abeae9b73dc9161778c5d4
JavaBool <javabooleanmc@gmail.com>
2026-01-13 15:13:23 +0530

    Added custom messages to select voters

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from werkzeug.security import generate_password_hash
 login_manager = LoginManager()
 login_manager.login_view = 'admin.login'
 
-
+#app
 @login_manager.user_loader
 def load_user(user_id):
     return Admin.query.get(int(user_id))
